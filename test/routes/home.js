08911673f15085275454448dfa3c3bb3fdac156d
Matt Hernandez <matt@modulus.io>
2015-09-11 12:35:44 -0400

    Update lint configuration
    Updated to use the Modulus eslint configuration.

--- test/routes/home.js
+++ test/routes/home.js
@@ -1,13 +1,13 @@
 const Code = require('code');
 const Lab = require('lab');
 
-var lab = exports.lab = Lab.script();
+const Server = require('../../');
 
-var server = require('../../');
+var lab = exports.lab = Lab.script();
 
 lab.describe('home route', function () {
   lab.it('should correctly render html', function (done) {
-    server.inject({ method: 'GET', url: '/' }, function (response) {
+    Server.inject({ method: 'GET', url: '/' }, function (response) {
       Code.expect(response.statusCode).to.equal(200);
       Code.expect(response.headers['content-type']).to.equal('text/html');
 
